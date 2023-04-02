a41a77d3bdb020b3e7102dc6d812a5b262b47dbe
“yeyoung8805” <yyk8805@naver.com>
2023-04-02 18:19:35 +0900

    [4_swift를 이용하여 구현하기] TabView 에 item 4개 추가

--- ch01_assetManagement/ContentView.swift
+++ ch01_assetManagement/ContentView.swift
@@ -1,9 +1,45 @@
 import SwiftUI
 
 struct ContentView: View {
+  @State private var selection: Tab = .asset
+
+  enum Tab {
+    case asset 
+    case recommend
+    case alert
+    case setting
+  }
+
   var body: some View {
-    Text("Hello, World!")
-      .padding()
+    TabView(selection: $selection) {
+      Color.white
+        .tabItem {
+          Image(systemName: "dollarsign.circle.fill")
+          Text("자산")
+        }
+        .tag(Tab.asset)
+      Color.blue
+        .edgesIgnoreingSafeArea(.all)
+        .tabItem {
+          Image(systemName: "hand.thumbsup.fill")
+          Text("추천")
+        }  
+        .tag(Tab.recommend)
+      Color.yellow
+        .edgesIgnoreingSafeArea(.all)
+        .tabItem {
+          Image(systemName: "bell.fill")
+          Text("알림")
+        }  
+        .tag(Tab.alert)
+      Color.red
+        .edgesIgnoreingSafeArea(.all)
+        .tabItem {
+          Image(systemName: "gearshape.fill")
+          Text("설정")
+        }  
+        .tag(Tab.setting)
+    }
   }
 }
 
