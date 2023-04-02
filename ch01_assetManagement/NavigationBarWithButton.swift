5a9483ff1781eb98d8a4c79f33bace7d47225e62
“yeyoung8805” <yyk8805@naver.com>
2023-04-02 18:22:43 +0900

    [5_Navigation 만들기] SwiftUIView 생성

---
+++ ch01_assetManagement/NavigationBarWithButton.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+
+struct NavigationBarWithButton: View {
+  var body: some View {
+    Text("Hello, World!")
+      .padding()
+  }
+}
+
+struct NavigationBarWithButton_Previews: PreviewProvider {
+  static var previews: some View {
+    NavigationBarWithButton()
+  }
+}
