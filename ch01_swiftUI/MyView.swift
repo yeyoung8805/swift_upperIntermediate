b78f795c74d0df89f95bb78bc58fd55666013d93
“yeyoung8805” <yyk8805@naver.com>
2023-04-02 17:04:14 +0900

    [swiftUI 알아보기] ContentView 에서 MyView 호출 적용

--- ch01_swiftUI/MyView.swift
+++ ch01_swiftUI/MyView.swift
@@ -2,18 +2,18 @@ import SwiftUI
 
 struct MyView: View {
   let helloFont: Font
+
   var body: some View {
     VStack {
       Text("Hello, World!")
-        .font(.title)
-      Text("만나서 반가워요!")
         .font(helloFont)
+      Text("만나서 반가워요!")
     }
   }
 }
 
 struct MyView_Previews: PreviewProvider {
   static var previews: some View {
-    MyView()
+    MyView(helloFont: .title)
   }
 }
