1d5863153d1a8d9613d5ca08e1e59dec655e6e3f
“yeyoung8805” <yyk8805@naver.com>
2023-04-02 20:03:35 +0900

    [7_배너 만들기] PageViewController 안에 각각 들어갈 pageControl 생성

---
+++ ch01_assetManagement/PageControl.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct PageControl: View {
+  var body: some View {
+    Text("Hello, World!")
+  }
+}
+
+struct PageControl_Previews: PreviewProvider {
+  static var previews: some View {
+    PageControl()
+  }
+}
