20c5970e4b2a53ee0054c8bf847fb63d56d77c8c
“yeyoung8805” <yyk8805@naver.com>
2023-04-02 19:20:54 +0900

    [6_GridView 만들기] SwiftUIView file 생성

---
+++ ch01_assetManagement/AssetView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct AssetView: View {
+  var body: some View {
+    Text("Hello, World!")
+  }
+}
+
+struct AssetView_Previews: PreviewProvider {
+  static var previews: some View {
+    AssetView()
+  }
+}
