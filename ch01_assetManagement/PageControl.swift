40aa70b09551c17a9883afa6c23e4f41e0163b11
“yeyoung8805” <yyk8805@naver.com>
2023-04-02 20:12:54 +0900

    [7_배너 만들기]UIKit 의 요소를 SwiftUI로 사용할 준비 완료

--- ch01_assetManagement/PageControl.swift
+++ ch01_assetManagement/PageControl.swift
@@ -1,13 +1,39 @@
 import SwiftUI
+import UIKit
 
-struct PageControl: View {
-  var body: some View {
-    Text("Hello, World!")
+struct PageControl: UIViewRepresentable {
+  var numberOfPages: Int
+  @Binding var currentPage: Int
+
+  func makeCoordinator() -> Coordinator {
+    Coordinator(self)
   }
-}
 
-struct PageControl_Previews: PreviewProvider {
-  static var previews: some View {
-    PageControl()
+  func makeUIView(context: Context) -> UIPageControl {
+    let control = UIPageControl()
+    control.numberOfPages = numberOfPages
+    control.addTarget(
+      context.coordinator,
+      action: #selector(Coordinator.updateCurrentPage(sender:)),
+      for: .valueChanged
+    )
+
+    return control
+  }
+
+  func updateUIView(_ uiView: UIPageControl, context: Context) {
+    uiView.currentPage = currentPage
+  }
+
+  class Coordinator: NSObject {
+    var control: PageControl
+
+    init(_ control: PageControl) {
+      self.control = control
+    }
+
+    @objc func updateCurrentPage(sender: UIPageControl) {
+      control.currentPage = sender.currentPage
+    }
   }
 }
