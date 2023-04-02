5ece017a7e6c774bb4290cd2cc2513abb606c554
“yeyoung8805” <yyk8805@naver.com>
2023-04-02 18:57:59 +0900

    [6_GridView 만들기] SwiftUIView 로 button style 커스텀하기 위해 생성

---
+++ ch01_assetManagement/AssetMenuButtonStyle.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct AssetMenuButtonStyle: View {
+  var body: some View {
+    Text("Hello, World!")
+  }
+}
+
+struct AssetMenuButtonStyle_Previews: PreviewProvider {
+  static var previews: some View {
+    AssetMenuButtonStyle()
+  }
+}
