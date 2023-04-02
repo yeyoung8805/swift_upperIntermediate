380cd203b1d21ea64eb20c721092da4a38db3bdc
“yeyoung8805” <yyk8805@naver.com>
2023-04-02 19:09:35 +0900

    [6_GridView 만들기] AssetMenu의 button style 커스텀함

--- ch01_assetManagement/AssetMenuButtonStyle.swift
+++ ch01_assetManagement/AssetMenuButtonStyle.swift
@@ -1,13 +1,46 @@
 import SwiftUI
 
-struct AssetMenuButtonStyle: View {
-  var body: some View {
-    Text("Hello, World!")
+struct AssetMenuButtonStyle: ButtonStyle {
+  let menu: AssetMenu
+  func makeBody(configuration: Configuration) -> some View {
+    return VStack {
+      Image(systemName: menu.systemImageName)
+        .resizable()
+        .frame(width: 30, height: 30)
+        .padding([.leading, .trailing], 10)
+      Text(menu.title)
+        .font(.system(size: 12, weight: .bold))
+    }
+    .padding()
+    .foregroundColor(.blue)
+    .background(Color.white)
+    .clipShape(RoundedRectangle(cornerRadius: 10))
   }
 }
 
 struct AssetMenuButtonStyle_Previews: PreviewProvider {
   static var previews: some View {
-    AssetMenuButtonStyle()
+    HStack(spacing: 24) {
+      Button("") {
+        print("")
+      }
+      .buttonStyle(AssetMenuButtonStyle(menu: .creditScore))
+
+      Button("") {
+        print("")
+      }
+      .buttonStyle(AssetMenuButtonStyle(menu: .bankAccount))
+
+      Button("") {
+        print("")
+      }
+      .buttonStyle(AssetMenuButtonStyle(menu: .creditCard))
+
+      Button("") {
+        print("")
+      }
+      .buttonStyle(AssetMenuButtonStyle(menu: .cash))
+    }
+    .background(Color.gray.opacity(0.2))
   }
 }
