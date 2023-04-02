81448a5ee8fa0ba21fd106bd0b083279362c1f0b
“yeyoung8805” <yyk8805@naver.com>
2023-04-02 18:43:56 +0900

    [5_Navigation 만들기] Button이 있는 Navagation Bar

--- ch01_assetManagement/NavigationBarWithButton.swift
+++ ch01_assetManagement/NavigationBarWithButton.swift
@@ -1,14 +1,57 @@
 import SwiftUI
 
-struct NavigationBarWithButton: View {
-  var body: some View {
-    Text("Hello, World!")
-      .padding()
+struct NavigationBarWithButton: ViewModifier {
+
+  var title: String = ""
+  func body(content: Content) -> some View {
+    return content
+      .navigationBarItems(
+        leading: Text(title)
+          .font(.system(size: 24, weight: .bold))
+          .padding(),
+        trailing: Button(
+          action: {
+            print("자산추가버튼 tapped")
+          },
+          label: {
+            Image(systemName: "plus")
+            Text("자산추가")
+              .font(.system(size: 12))
+          }
+        )
+        .accentColor(.black)
+        .padding(EdgeInsets(top: 8, leading: 8, bottom: 8, trailing: 8))
+        .overlay(
+          RoundedRectangle(cornerRadius: 10)
+            .stroke(Color.black)
+        )
+      )
+      .navigationBarTitleDisplayMode(.inline)
+      onAppear {
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithTranaparentBackground()
+        appearance.backgroundColor = UIColor(white: 1, alpha: 0.5)
+        UINavigationBar.appearance().standardAppearance = appearance
+        UINavigationBar.appearance().compactAppearance = appearance
+        UINavigationBar.appearance().scrollEdgeAppearance = appearance
+      }
   }
 }
 
+//View 가 함수를 사용할수 있게 한다.
+extension View {
+  func  navigationBarWithButtonStyle(_ title: String) -> some View {
+    return self.modifier(NavigationBarWithButton(title: title))
+  }
+}
+
+
 struct NavigationBarWithButton_Previews: PreviewProvider {
   static var previews: some View {
-    NavigationBarWithButton()
+    NavigationView {
+      Color.gray.edgesIgnoreingSafeArea(.all)
+        // .navigationBarWithButtonStyle
+        .navigationBarWithButtonStyle("내 자산")
+    }
   }
 }
