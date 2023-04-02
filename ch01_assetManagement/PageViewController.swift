cf8bb2301793e8cdfe7b86fffe518e4105476023
“yeyoung8805” <yyk8805@naver.com>
2023-04-02 19:41:16 +0900

    [7_배너 만들기] SwiftUIView 생성

---
+++ ch01_assetManagement/PageViewController.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct PageViewController: View {
+  var body: some View {
+    Text("Hello, World!")
+  }
+}
+
+struct PageViewController_Previews: PreviewProvider {
+  static var previews: some View {
+    PageViewController()
+  }
+}
