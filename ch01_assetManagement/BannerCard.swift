cb39fa378b84e6732a5f2aaaa2f34f2a2253d09f
“yeyoung8805” <yyk8805@naver.com>
2023-04-02 19:39:42 +0900

    [7_배너 만들기] BannerCard (title,description)

--- ch01_assetManagement/BannerCard.swift
+++ ch01_assetManagement/BannerCard.swift
@@ -1,13 +1,37 @@
 import SwiftUI
 
 struct BannerCard: View {
+  var banner: AssetBanner
   var body: some View {
-    Text("Hello, World!")
+    //방법1 다른 표현 방법
+    // ZStack {
+    //   Color(banner.backgoundColor)
+    //   VStack {
+    //       Text(banner.title)
+    //         .font(.title)
+    //       Text(banner.description)
+    //         .font(.subheadline)
+    //     }
+    // }
+
+    //방법2
+    Color(banner.backgoundColor)
+      .overlay(
+        VStack {
+          Text(banner.title)
+            .font(.title)
+          Text(banner.description)
+            .font(.subheadline)
+        }
+      )
   }
 }
 
 struct BannerCard_Previews: PreviewProvider {
   static var previews: some View {
-    BannerCard()
+    let banner0 = AssetBanner(title: "공지사항", 
+      description: "추가된 공지사항을 확인하세요.", 
+      backgroundColor: .red)
+    BannerCard(banner: banner0)
   }
 }
