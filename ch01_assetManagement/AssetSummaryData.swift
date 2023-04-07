9441fb48c421e26e1691897c87e998914d678b31
yeyoung8805 <yyk8805@naver.com>
2023-04-07 15:41:08 +0900

    [8_자산관리 리스트 만들기 1] AssetSectionHeaderView(swiftUIView) 생성

--- ch01_assetManagement/AssetSummaryData.swift
+++ ch01_assetManagement/AssetSummaryData.swift
@@ -1,13 +1,27 @@
 import SwiftUI
 
-struct AssetBannerView: View {
-  var body: some View {
-    Text("Hello, World!")
-  }
+class AssetBannerView: ObservableObject {
+  @Published var assets: [Asset] = load("assets.json")
+
 }
 
-struct AssetBannerView_Previews: PreviewProvider {
-  static var previews: some View {
-    AssetBannerView()
+func load<T: Decodable>(_ filename: String) -> T {
+  let data: Data
+
+  guard let file = Bundle.main.url(forResource: filename, withExtension: nil) else {
+    fatalError(filename + "을 찾을 수 없습니다.")
+  }
+
+  do {
+    data = try Data(contentsOf: file)
+  } catch {
+    fatalError(filename + "을 찾을 수 없습니다.")
+  }
+
+  do {
+    let decoder = JSONDecoder()
+    return try decoder.decode(T.self, from: data)
+  } catch {
+    fatalError(filename + "을 \(T.self)로 파싱할 수 없습니다.")
   }
 }
