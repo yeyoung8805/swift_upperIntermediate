287fe6fa5bdc10058642d9e39ee8daefbf42a911
yeyoung8805 <yyk8805@naver.com>
2023-04-07 15:31:45 +0900

    [8_자산관리 리스트 만들기 1] AssetSummaryData (SwiftUIView) 생성

---
+++ ch01_assetManagement/AssetSummaryData.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct AssetBannerView: View {
+  var body: some View {
+    Text("Hello, World!")
+  }
+}
+
+struct AssetBannerView_Previews: PreviewProvider {
+  static var previews: some View {
+    AssetBannerView()
+  }
+}
