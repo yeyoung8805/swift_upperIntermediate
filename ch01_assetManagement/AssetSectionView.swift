b46a6c430a84333c92eacc967fa2b5c1affc582d
yeyoung8805 <yyk8805@naver.com>
2023-04-07 16:43:40 +0900

    [8_자산관리 리스트 만들기 1] AssetSectionView : 데이터와 HStack

--- ch01_assetManagement/AssetSectionView.swift
+++ ch01_assetManagement/AssetSectionView.swift
@@ -1,13 +1,39 @@
 import SwiftUI
 
 struct AssetSectionView: View {
+  @ObservedObject var assetSection: Asset
   var body: some View {
-    Text("Hello, World!")
+    VStack(spacing: 20) {
+      AssetSectionHeaderView(title: assetSection.type.title)
+      ForEach(assetSection.data) { asset in
+        HStack {
+          Text(asset.title)
+            .font(.title)
+            .foregroundColor(.secondary)
+          Spacer()
+          Text(asset.amount)
+            .font(.title2)
+            .foregroundColor(.primary)
+        }
+        Divider()
+
+      }
+    }
+    .padding()
   }
 }
 
 struct AssetSectionView_Previews: PreviewProvider {
   static var previews: some View {
-    AssetSectionView()
+    let asset = Asset(
+      id: 0,
+      type: .bankAccount,
+      data: [
+        AssetData(id: 0, title: "신한은행", amount: "5,300,000원"),
+        AssetData(id: 1, title: "국민은행", amount: "15,300,000원"),
+        AssetData(id: 2, title: "카카오은행", amount: "25,300,000원")
+      ]
+    )
+    AssetSectionView(assetSection: asset)
   }
 }
