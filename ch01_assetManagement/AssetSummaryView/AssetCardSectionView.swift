9cd7e446db32eb755538fe6c7d9bf1cc669c62e5
“yeyoung8805” <yyk8805@naver.com>
2023-04-09 17:30:11 +0900

    [9_자산관리 리스트 만들기 2]  Header 하위 CardSection 그리기

--- ch01_assetManagement/AssetSummaryView/AssetCardSectionView.swift
+++ ch01_assetManagement/AssetSummaryView/AssetCardSectionView.swift
@@ -1,13 +1,52 @@
 import SwiftUI
 
 struct AssetCardSectionView: View {
+  @State private var  selectedTab = 1
+  @ObservedObject var asset: Asset
+
+  var assetData: [AssetData] {
+    return asset.data
+  }
+
   var body: some View {
-    Text("Hello, World!")
+    VStack {
+      AssetSectionHeaderView(title: asset.type.title)
+      TabMenuView {
+        tabs: ["지난달 결제", "이번달 결제", "다음달 결제"],
+        selectedTab: $selectedTab,
+        updated: .constant(.nextMonth(amount: "10,000원"))
+      }
+      TabView(
+        selection: $selectedTab,
+        content: {
+          ForEach(0...2, id: \.self) { i in
+            VStack {
+              ForEach(assetData) { data in
+                HStack {
+                  Text(data.title)
+                    .font(.title)
+                    .foregroundColor(.secondary)
+                  Spacer()
+                  Text(date.creditCardAmounts![i].amount)
+                    .font(.title2)
+                    .foregroundColor(.primary)
+                }
+                Divider()
+              }
+            }
+            .tag(i)
+          }
+        }
+      )
+      .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+    }
+    .padding()
   }
 }
 
 struct AssetCardSectionView_Previews: PreviewProvider {
   static var previews: some View {
-    AssetCardSectionView()
+    let asset = AssetSummaryData().assets[5]
+    AssetCardSectionView(asset: asset)
   }
 }
