afa30413c90c41c6e4d65f4031b04be8d908b5b2
“yeyoung8805” <yyk8805@naver.com>
2023-04-09 14:17:37 +0900

    [9_자산관리 리스트 만들기 2] TabMenuView(SwiftUIView) 생성

---
+++ ch01_assetManagement/AssetSummaryView/TabMenuView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct TabMenuView: View {
+  var body: some View {
+    Text("Hello, World!")
+  }
+}
+
+struct TabMenuView_Previews: PreviewProvider {
+  static var previews: some View {
+    TabMenuView()
+  }
+}
