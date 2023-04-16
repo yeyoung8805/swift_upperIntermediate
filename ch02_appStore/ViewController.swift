a861b158ee795840f9c404a30b1abd8a7983a8ab
“yeyoung8805” <yyk8805@naver.com>
2023-04-16 19:11:35 +0900

    [1_앱스토어 투데이 Tab 구현하기] TabBar UI 구성완료

--- ch02_appStore/ViewController.swift
+++ ch02_appStore/ViewController.swift
@@ -4,11 +4,23 @@ class TabBarController: UITabBarController {
 
   private lazy var todayViewController: UIViewController = {
     let viewController = UIViewController()
+    let tabBarItem = UITabBarItem(
+        title: "투데이", 
+        image: UIImage(systemName: "mail"), 
+        tag: 0)
+    viewController.tabBarItem = tabBarItem    
+
     return viewController
   }()
 
   private lazy var appViewController: UIViewController = {
     let viewController = UIViewController()
+    let tabBarItem = UITabBarItem(
+        title: "앱", 
+        image: UIImage(systemName: "square.stack.3d.up"), 
+        tag: 1)
+    viewController.tabBarItem = tabBarItem 
+
     return viewController
   }()
 
