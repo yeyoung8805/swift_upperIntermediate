8c2e359d973c26e5493126de419f2cbfa2c529c9
“yeyoung8805” <yyk8805@naver.com>
2023-04-23 16:29:00 +0900

    [2_앱스토어 앱 Tab 구현하기]내비게이션바에 "앱" 표시되게 함

--- ch02_appStore/Scenes/App/AppViewController.swift
+++ ch02_appStore/Scenes/App/AppViewController.swift
@@ -2,5 +2,19 @@ import SnapKit
 import UIKit
 
 final class AppViewController: UIViewController {
-  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    setupNavigationController()
+  }
+ 
+
+}
+
+private extension AppViewController {
+  func setupNavigationController() {
+    navigationItem.title = "앱"
+    navigationItem.largeTitleDisplayMode = .always
+    navigationController?.navigationBar.prefersLargeTitles = true       
+  }
 }
