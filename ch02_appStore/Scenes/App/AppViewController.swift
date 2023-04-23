f2d145615815eccf030d4cadcde8985c57bc30a6
“yeyoung8805” <yyk8805@naver.com>
2023-04-23 16:42:30 +0900

    [2_앱스토어 앱 Tab 구현하기] subView로 scrollView 추가

--- ch02_appStore/Scenes/App/AppViewController.swift
+++ ch02_appStore/Scenes/App/AppViewController.swift
@@ -2,6 +2,18 @@ import SnapKit
 import UIKit
 
 final class AppViewController: UIViewController {
+  private let scrollView = UIScrollView()
+  private let contentView = UIView()
+
+  private lazy var stackView: UIStackView = {
+    let stackView = UIStackView()
+    stackView.axis = .vertical
+    stackView.distribution = .equalSpacing
+    stackView.spacing = 0.0
+
+    return stackView
+  }()
+
   override func viewDidLoad() {
     super.viewDidLoad()
 
@@ -16,5 +28,27 @@ private extension AppViewController {
     navigationItem.title = "앱"
     navigationItem.largeTitleDisplayMode = .always
     navigationController?.navigationBar.prefersLargeTitles = true       
+  }
+
+  func setupLayout() {
+    view.addSubView(scrollView)
+    scrollView.snp.makeConstraints {
+      $0.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+      $0.bottom.equalToSuperView()
+      $0.leading.equalToSuperView()
+      $0.trailing.equalToSuperView()
+    }
+
+    scrollView.addSubView(contentView)
+    contentView.snp.makeConstraints {
+      $0.edges.equalToSuperView()
+      $0.width.equalToSuperView() //세로로 스크롤이 되도록 함.. height 일 경우 가로스크롤
+    }
+
+    contentView.addSubView(stackView)
+    stackView.snp.makeConstraints {
+      $0.edges.equalToSuperView() //모서리들을 contentView에 맞춘다.
+      
+    }
   }
 }
