c9266f732efdf1f415ae76d8d7a7bb9f1a5eb27d
“yeyoung8805” <yyk8805@naver.com>
2023-05-01 16:27:40 +0900

    [3_앱스토어 앱 상세화면 구현하기]  5가지 UIComponent property 추가

--- ch02_appStore/Scenes/AppDetailViewController.swift
+++ ch02_appStore/Scenes/AppDetailViewController.swift
@@ -2,6 +2,50 @@ import SnapKit
 import UIKit
 
 final class AppDetailViewController: UIViewController {
+  private let appIconImageView: UIImageView = {
+    let imageView = UIImageView()
+    imageView.contentMode = .scaleAspectFill
+    imageView.clipsToBounds = true
+    imageView.layer.cornerRadius = 8.0
+
+    return imageView
+  }()
+
+  private let titleLabel: UILabel = {
+    let label = UILabel()
+    label.font = .systemFont(ofSize: 20.0, weight: .bold)
+    label.textColor = .label
+
+    return label
+  }()
+
+  private let subTitleLabel: UILabel = {
+    let label = UILabel()
+    label.font = .systemFont(ofSize: 14.0, weight: .medium)
+    label.textColor = .secondaryLabel
+
+    return label
+  }()
+
+  private let downloadButton: UIButton = {
+    let button = UIButton()
+    button.setTitle("받기", for: .normal)
+    button.titleLabel?.font = .systemFont(ofSize: 13.0, weight: bold)
+    button.backgroundColor = .systemBlue
+    button.setTitleColor(.white, for: .normal)
+    button.layer.cornerRadius = 12.0
+
+    return button
+  }()
+
+  private let shareButton: UIButton = {
+    let button = UIButton()
+    button.setImage(UIImage(systemName: "square.and.arrow.up"), for: .normal)
+    button.tintColor = .systemBlue
+
+    return button
+  }()
+
   override func viewDidLoad() {
     super.viewDidLoad()
 
