f732cdf75cb101118dc31e5b141961a2275bbc57
“yeyoung8805” <yyk8805@naver.com>
2023-05-15 05:20:04 +0900

    [2_도착정보 화면 구현하기] collectionView 3개만 임의로 구현

--- ch03_subwayStation/Scenes/StationDetailViewController.swift
+++ ch03_subwayStation/Scenes/StationDetailViewController.swift
@@ -2,5 +2,44 @@ import SnapKit
 import UIKit
 
 final class StationDetailViewController: UIViewController {
-  
+  private lazy var collectionView: UICollectionView = {
+    let layout = UICollectionViewFlowLayout()
+    layout.estimatedItemSize = CGSize(
+      width: view.frame.width - 32.0, 
+      height: 100.0)
+    layout.sectionInset = UIEdgeInsets(top: 16.0, left: 16.0, bottom: 16.0, right: 16.0)
+    layout.scrollDirection = .vertical
+
+    let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+    collectionView.backgroundColor = .systemBackground
+    collectionView.register(UICollectionViewCell.self,
+      forCellWithReuseIdentifier: "StationDetailCollectionViewCell")
+
+    collectionView.dataSource = self
+    
+    return collectionView
+  }
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    navigationItem.title = "왕십리"
+
+    view.addSubview(collectionView)
+    collectionView.snp.makeConstraints { $0.edges.equalToSuperView() }
+  }
+}
+
+extension StationDetailViewController: UICollectionViewDataSource {
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return 3
+  } 
+
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "StationDetailCollectionViewCell", for: indexPath)
+
+    cell.backgroundColor = .gray
+
+    return cell
+  } 
 }
