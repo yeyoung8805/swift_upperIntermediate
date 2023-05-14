227c5a086bdd90bca93ab766ed340d5de7c1dda0
“yeyoung8805” <yyk8805@naver.com>
2023-05-15 04:55:05 +0900

    [1_지하철역 검색화면 구현하기] StationSearchViewController 상세

--- ch03_subwayStation/StationSearchViewController.swift
+++ ch03_subwayStation/StationSearchViewController.swift
@@ -2,16 +2,58 @@ import SnapKit
 import UIKit
 
 class StationSearchViewController: UIViewController {
+  private lazy var tableView: UITableView = {
+    let tableView = UITableView()
+    tableView.dataSource = self
+    tableView.isHidden = true
+
+    return tableView()
+  }
+
   override func viewDidLoad() {
     super.viewDidLoad()
 
+    setNavigationItems()
+    setTableViewLayout()
+  }
+
+  private func setNavigationItems() {
     navigationController?.navigationBar.prefersLargeTitles = true
     navigationItem.title = "지하철 도착 정보"
 
     let searchController = UISearchController()
     searchController.searchBar.placeholder = "지하철 역을 입력해주세요."
-    searchController.obscuresBackgroundDuringPresentation = false //TODO true 일 때 와의 차이 비교
+    searchController.obscuresBackgroundDuringPresentation = false //TODO true 일 때 와의 차이 비교 (false = 회색 어둡게 하지 않음)
+    searchController.searchBar.delegate = self
 
     navigationItem.searchController = searchController
+  }
+
+  private func setTableViewLayout() {
+    view.addSubview(tableView)
+    tableView.snp.makeConstraints{ $0.edges.equalToSuperView() }
+  }
+}
+
+extension StationSearchViewController: UISearchBarDelegate {
+  func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+    tableView.isHidden = false // tableView 가 보이기 시작
+  }
+
+  func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+    tableView.isHidden = true // tableView 가 보이지 않게 됨
+  }
+}
+
+extension StationSearchViewController: UITableViewDataSource {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 10 // 임의의 셀 개수 10개
+  }
+
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = UITableViewCell()
+    cell.textLabel?.text = "\(indexPath.item)"
+
+    return cell
   }
 }
