157d9f34dd8c34fa5ac64f48b089bb8176436e1c
“yeyoung8805” <yyk8805@naver.com>
2023-06-11 17:35:59 +0900

    [1_인스타그램 피드화면 구현하기] tableView 10개 일단 추가

--- ch04_instagram/Scenes/FeedViewController.swift
+++ ch04_instagram/Scenes/FeedViewController.swift
@@ -2,10 +2,32 @@ import SnapKit
 import UIKit
 
 class FeedViewController: UIViewController {
+  private lazy var tableView: UITableView = {
+    let tableView = UITableView(frame: .zero)
+    tableView.backgroundColor = .systemBackground
+    tableView.seperatorStyle = .none
+    tableView.dataSource = self
+
+    return tableView
+  }()
+
   override func viewDidLoad() {
     super.viewDidLoad()
 
     setupNavigationBar()
+    setupTableView()
+  }
+}
+
+extension FeedViewController: UITableViewDataSource {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 10
+  }
+
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = UITableViewCell()
+    cell.backgroundColor = .black
+    return cell
   }
 }
 
@@ -20,5 +42,10 @@ private extension FeedViewController {
       action: nil
     )
     navigationItem.rightBarButtonItem = uploadButton
+  }
+
+  func setupTableView() {
+    view.addSubView(tableView)
+    tableView.snp.makeConstraints { $0.edges.equalToSuperview() }
   }
 }
