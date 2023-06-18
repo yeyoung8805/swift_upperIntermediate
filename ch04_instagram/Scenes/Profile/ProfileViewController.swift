b64e599a3ea67b1a787a2ef5d13f725df8d7e4d3
“yeyoung8805” <yyk8805@naver.com>
2023-06-18 19:29:04 +0900

    [2_프로필 화면 구현하기]  collectionView(이미지목록) 그리기

--- ch04_instagram/Scenes/Profile/ProfileViewController.swift
+++ ch04_instagram/Scenes/Profile/ProfileViewController.swift
@@ -58,11 +58,44 @@ final class ProfileViewController: UIViewController {
   private let followerDataView = ProfileDataView(title: "팔로워", count: 2_000)
   private let followingDataView = ProfileDataView(title: "팔로잉", count: 1)
 
+  private lazy var collectionView: UICollectionView = {
+    let layout = UICollectionViewFlowLayout()
+
+    let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+    collectionView.backgroundColor = .systemBackground
+    collectionView.register(UICollectionView.self, forCellWithReuseIdentifier: "ProfileCollectionViewCell")
+    collectionView.datasource = self
+
+    return collectionView
+  }()
+
   override func viewDidLoad() {
     super.viewDidLoad()
 
     setupNavigationItems()
     setupLayout()
+  }
+}
+
+//delegate
+//datasource
+
+extension ProfileViewController: UICollectionViewDataSource {
+  func collectionView(_ collectionView: UICollectionView,
+    cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+      let cell = collectionView
+        .dequeueReusableCell(
+          withReuseIdentifier: "ProfileCollectionViewCell", 
+          for: indexPath
+        )
+
+      cell.backgroundColor = .lightGray
+      return cell
+  }
+
+  func collectionView(_ collectionView: UICollectionView,
+    numberOfItemsInSection section: Int) -> Int {
+      return 10
   }
 }
 
@@ -91,7 +124,7 @@ private extension ProfileViewController {
     dataStackView.distribution = .fillEqually
 
     [
-      profileImageView, dataStackView, nameLabel, descriptionLabel, buttonStackView
+      profileImageView, dataStackView, nameLabel, descriptionLabel, buttonStackView, collectionView
     ].forEach{ view.addSubView($0) }
 
     let inset: CGFloat = 16.0
@@ -125,6 +158,13 @@ private extension ProfileViewController {
       $0.top.euqalTo(descriptionLabel.snp.bottom).offset(12.0)
       $0.leading.euqalTo(nameLabel.snp.leading)
       $0.trailing.equalTo(nameLabel.snp.trailing)
+    }
+
+    collectionView.snp.makeConstraints {
+      $0.leading.equalToSuperview()
+      $0.trailing.equalToSuperview()
+      $0.top.equalTo(buttonStackView.snp.button).offset(16.0)
+      $0.bottom.equalToSuperview()
     }
 
   }
