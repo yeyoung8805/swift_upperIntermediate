4ebc0b36e4a23b629ae32c471d2b83f36fa9e1e2
“yeyoung8805” <yyk8805@naver.com>
2023-06-24 20:00:30 +0900

    [4_인스타그램 게시물 업로드 기능 구현하기] imageView, textView 위치 잡아줌

--- ch04_instagram/Scenes/Feed/UploadViewController.swift
+++ ch04_instagram/Scenes/Feed/UploadViewController.swift
@@ -2,11 +2,21 @@ import SnapKit
 import UIKit
 
 final class UploadViewController: UIViewController {
+  private let imageView = UIImageView()
+
+  private lazy var textView: UITextView = {
+    let textView = UITextView()
+    textView.font = .systemFont(ofSize: 15.0)
+
+    return textView
+  }()
+
   override func viewDidLoad() {
     super.viewDidLoad()
 
     view.backgroundColor = .systemBackground
     setupNavigationItem()
+    setupLayout()
   }
 }
 
@@ -35,4 +45,25 @@ private extension UploadViewController {
     //TODO: 저장하기 기능추가 필요
     dismiss(animated: true)
   }
+
+  func setupLayout() {
+    [imageView, textView].forEach { view.addSubview($0) }
+
+    let imageViewInset: CGFloat = 16.0
+
+    imageView.snp.makeConstraints {
+      $0.top.equalTo(view.safeAreaLayoutGuide).inset(imageViewInset)
+      $0.leading.equalToSuperview().inset(imageViewInset)
+      $0.width.equalTo(100.0)
+      $0.height.equalTo(imageView.snp.width)
+    }
+
+    textView.snp.makeConstraints {
+      $0.leading.equalTo(imageView.snp.trailing).offset(imageViewInset)
+      $0.trailing.equalToSuperview().inset(imageViewInset)
+      $0.top.equalTo(imageView.snp.top)
+      $0.bottom.equalTo(imageView.snp.bottom)
+    }
+  }
+
 }
