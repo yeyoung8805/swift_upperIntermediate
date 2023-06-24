e0436d9e7bd4c3500a17b719af426f900536d694
“yeyoung8805” <yyk8805@naver.com>
2023-06-24 19:48:16 +0900

    [4_인스타그램 게시물 업로드 기능 구현하기] setupNavigationItem()

--- ch04_instagram/Scenes/Feed/UploadViewController.swift
+++ ch04_instagram/Scenes/Feed/UploadViewController.swift
@@ -5,6 +5,34 @@ final class UploadViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
 
-    view.backgroundColor = .systemRed
+    view.backgroundColor = .systemBackground
+    setupNavigationItem()
+  }
+}
+
+private extension UploadViewController {
+  func setupNavigationItem() {
+    navigationItem.title = "새 게시물"
+    navigationItem.leftBarButtonItem = UIBarButtonItem(
+      title: "취소", 
+      style: .plain, 
+      target: self, 
+      action: #selector(didTapLeftButton)
+    )
+    navigationItem.rightBarButtonItem = UIBarButtonItem(
+      title: "공유", 
+      style: .plain, 
+      target: self, 
+      action: #selector(didTapRightButton)
+    )
+  }
+
+  @objc func didTapLeftButton() {
+    dismiss(animated: true)
+  }
+
+  @objc func didTapRightButton() {
+    //TODO: 저장하기 기능추가 필요
+    dismiss(animated: true)
   }
 }
