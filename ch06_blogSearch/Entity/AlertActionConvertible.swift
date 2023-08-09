6f9764e5905b0f334dff2504fd467d4846245331
yeyoung8805 <yyk8805@naver.com>
2023-08-09 15:57:25 +0900

    [5_기본 UI 그리기]  alertActionTapped 되었을때 처리

---
+++ ch06_blogSearch/Entity/AlertActionConvertible.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+protocol AlertActionConvertible {
+  var title: String { get }
+  var style: UIAlertAction.Style { get }
+  
+}
--- ch06_blogSearch/Presentation/MainViewController/MainViewController.swift
+++ ch06_blogSearch/Presentation/MainViewController/MainViewController.swift
@@ -8,6 +8,8 @@ class MainViewController: UIViewController {
   let searchBar = SearchBar()
   let listView = BlogListView()
 
+  let alertActionTapped = PublishRelay<AlertAction>()
+
   override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
     super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
 
@@ -21,7 +23,19 @@ class MainViewController: UIViewController {
   }
 
   private func bind() {
-    
+    let alertSheetForSorting = listView.headerView.sortButtonTapped
+      .map { _ -> Alert in
+        return (title: nil, message: nil, actions: [.title, .datetime, .cancel], style: .actionSheet)
+      }
+
+    alertSheetForSorting
+      .asSignal(onErrorSignalWith: .empty())
+      .flatMapLatest { alert -> Signal<AlertAction> in
+        let alertController = UIAlertController(title: alert.title, message: alert.message, preferredStyle: alert.style)
+        return self.presentAlertController(alertController, actions: alert.actions)
+      }
+      .emit(to: alertActionTapped)
+      .disposed(by: disposeBag)
   }
 
   private func attribute() {
@@ -30,7 +44,76 @@ class MainViewController: UIViewController {
   }
 
   private func layout() {
+    [searchBar, listView].forEach { view.addSubview($0) }
 
+    searchBar.snp.makeConstraints {
+      $0.top.equalTo(view.safeAreaLayoutGuide)
+      $0.leading.trailing.equalToSuperview()
+    }
+
+    listView.snp.makeConstraints {
+      $0.top.equalTo(searchBar.snp.bottom)
+      $0.leading.trailing.bottom.equalToSuperview()
+    }
+  }
+
+}
+
+//Alert 을 extension 으로 분리함
+extension  MainViewController {
+  typealias Alert = (title: String?, message: String?, actions: [AlertAction], style: UIAlertController.style)
+
+  enum AlertAction: AlertActionConvertible {
+    case title, datetime, cancel
+    case confirm
+
+    var title: String {
+      switch self {
+        case .title:
+          return "Title"
+        case .datetime:
+          return "Datetime"
+        case .cancel:
+          return "취소"
+        case .confirm:
+          return "확인"
+      }
+    }
+
+    var style: UIAlertAction.Style {
+      switch self {
+        case .title, .datetime:
+          return .default
+        case .cancel, .confirm:
+          return .cancel
+      }
+    }
+
+  }
+
+  func presentAlertController<Action: AlertActionConvertible>(_ alertController: UIAlertController, actions: [Action]) -> Signal<Action> {
+    if actions.isEmpty { return .empty() }
+    return Observable
+      .create{[weak self] observer in
+        guard let self = self else { return Disposables.create() }
+        for action in actions {
+          alertController.addAction(
+            UIAlertAction(
+              title: action.title,
+              style: action.style,
+              handler: { _ in
+                observer.onNext(action)
+                observer.onComplete()
+            })
+          )
+        }
+        self.present(alertController, animated: true, completion: nil)
+
+        return Disposables.create {
+          alertController.dismiss(animated: true, completion: nil)
+        }
+      }
+      .asSignal(onErrorSignalWith: .empty())
   }
 
 }
