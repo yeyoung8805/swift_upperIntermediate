05a65ec4e21ff64bd59d1fcd51581963160c7127
yeyoung8805 <yyk8805@naver.com>
2023-08-09 15:18:42 +0900

    [5_기본 UI 그리기] 페이지 생성 및 폴더 구분

---
+++ ch06_blogSearch/Entity/BlogListCellData.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+struct BlogListCellData {
+  let thumbnailURL: URL?
+  let name: String?
+  let title: String?
+  let datetime: Date?
+}
+
--- ch06_blogSearch/Presentation/MainViewController/MainViewController.swift
+++ ch06_blogSearch/Presentation/MainViewController/MainViewController.swift
@@ -5,8 +5,8 @@ import RxCocoa
 class MainViewController: UIViewController {
   let disposeBag = DisposeBag()
 
-  //let listView
-  //let searchBar
+  let searchBar = SearchBar()
+  let listView = BlogListView()
 
   override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
     super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
@@ -21,7 +21,7 @@ class MainViewController: UIViewController {
   }
 
   private func bind() {
-
+    
   }
 
   private func attribute() {
