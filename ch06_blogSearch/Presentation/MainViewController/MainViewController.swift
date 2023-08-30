4d22969fd30bb77092eb538fcb57fce766a6c9de
yeyoung8805 <yyk8805@naver.com>
2023-08-30 16:55:55 +0900

    [7_데이터 처리하기] alertForErrorMessage  로 블로그에러 처리

--- ch06_blogSearch/Presentation/MainViewController/MainViewController.swift
+++ ch06_blogSearch/Presentation/MainViewController/MainViewController.swift
@@ -29,31 +29,88 @@ class MainViewController: UIViewController {
       }
       .share()
 
-      let blogValue = blogResult
-        .compactMap { data-> DKBlog? in
-          guard case .success(let value) = data else {
-            return nil
-          }
-          return value
+    let blogValue = blogResult
+      .compactMap { data-> DKBlog? in
+        guard case .success(let value) = data else {
+          return nil
         }
+        return value
+      }
 
-      let blogError = blogResult
-        .compactMap { data -> String? in
-          guard case .failure(let error) = data else {
-            return nil
-          }
-          return error.localizedDescription
+    let blogError = blogResult
+      .compactMap { data -> String? in
+        guard case .failure(let error) = data else {
+          return nil
         }
+        return error.localizedDescription
+      }
 
-      //네트워크를 통해 가져온 값을 cellData 로 변환
+    //네트워크를 통해 가져온 값을 cellData 로 변환
+    let cellData = blogValue
+      .map { blog -> [BlogListCellData] in
+        return blog.documents
+          .map { doc in
+            let thumbnailURL = URL(string: doc.thumbnail ?? "")
+            return BlogListCellData(
+              thumbnailURL: thumbnailURL,
+              name: doc.name,
+              title: doc.title,
+              datetime: doc.datetime
+            )
+          }
+      }
 
+    //FilterView를 선택했을때 나오는 alertsheet를 선택했을때 type
+    let sortedType = alertActionTapped
+      .filter {
+        switch $0 {
+          case .title, .datetime:
+            return true
+          default:
+            return false
+        }
+      }
+      .startWith(.title)
+
+    //MainViewController -> ListView 로 전달 필요
+    Observable
+      .combineLatest(
+        sortedType,
+        cellData
+      ) { type, data -> [BlogListCellData] in
+          switch type {
+            case .title:
+              return data.sorted { $0.title ?? "" < $1.title ?? "" }
+            case .datetime:
+              return data.sorted { $0.datetime ?? Date() > $1.datetime ?? Date() }
+            default:
+              return data
+          }
+      }
+      .bind(to: listView.cellData)
+      .disposed(by: disposeBag)
 
     let alertSheetForSorting = listView.headerView.sortButtonTapped
       .map { _ -> Alert in
         return (title: nil, message: nil, actions: [.title, .datetime, .cancel], style: .actionSheet)
       }
 
-    alertSheetForSorting
+    //위 blogError에 대한 처리도 필요하다.
+    let alertForErrorMessage = blogError
+      .map { message -> Alert in
+        return (
+          title: "앗!",
+          message: "예상치 못한 오류가 발생했습니다. 잠시후 다시 시도해주세요. \(message)",
+          actions: [.confirm],
+          style: .alert
+        )
+      }
+
+    Observable
+      .merge(
+        alertSheetForSorting,
+        alertForErrorMessage
+      )
       .asSignal(onErrorSignalWith: .empty())
       .flatMapLatest { alert -> Signal<AlertAction> in
         let alertController = UIAlertController(title: alert.title, message: alert.message, preferredStyle: alert.style)
